2e97290f60089977cce876d0bf1899e5be78389d
Student <Student@10.2.22.54>
2016-04-15 15:04:11 +0200

    refactrored

--- src/nl/youngcapital/atm/elements/Air.java
+++ src/nl/youngcapital/atm/elements/Air.java
@@ -3,8 +3,8 @@ package nl.youngcapital.atm.elements;
 public class Air extends Element {
 	private Element strengthElement;
 	private Element weaknessElement;
+	
 	static final private String NAME = "Air";
-
 	static final private Element DEFAULT_STRENGHT_ELEMENT = new Fire();
 	static final private Element DEFAULT_WEAKNESS_ELEMENT = new Earth();
 	
